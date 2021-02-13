94423d04823c344c05e238b0673bf2643e9c8bdc
lastar38 <lastarmain@gmail.com>
2021-02-13 16:44:40 +0900

    完成

---
+++ SongTime/SongTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var railgun = new Song("only my railgun", "fripSide", 255);
+            var kizyuri = new Song("Love wit you", "fripSide", 302);
+            var Kings = new Song("legendary future", "fripSide", 298);
+            var oni = new Song("Riging Hope", "LiSA", 242);
+            var sao = new Song("crossing field", "LiSA", 251);
+
+            var songs = new Song[5] { railgun, kizyuri, Kings, oni, sao};
+
+            foreach (var song in songs)
+            {
+                int min = song.Time / 60;
+                int sec = song.Time % 60;
+                Console.WriteLine($"{song.Title} {min}:{sec}");
+            }
+        }
+    }
+}
---
+++ SongTime/SongTime/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongTime
+{
+    class Song
+    {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Time { get; set; }
+
+        public Song(string title, string artistname, int time)
+        {
+            Title = title;
+            ArtistName = artistname;
+            Time = time;
+        }
+    }
+}
